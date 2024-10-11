156311a20ea732ea3861aac275c497596c9522ae
Darell Macwan <macwandarell@gmail.com>
2024-10-11 15:06:37 +0530

    added OrderManager class in file manager module
    Order Manager class consists of the methods to add new orders to the orders.csv file, read orders of all the customers or a singular customer and a method which will be used to display the order

--- file_manager.py
+++ file_manager.py
@@ -1,5 +1,7 @@
 import csv
 
+#This class is to manage all the login stuff related to the customers
+
 class CustomerManager:
 
     def __init__(self,file='customers.csv'):
@@ -26,7 +28,7 @@ class CustomerManager:
             """This method will write a single row in the csv file
                and the single row is the list that will be written"""
             
-    #The below method will convert the csv into a readable format for Python
+    #The below method will convert the csv into a list format
 
     def read_customers(self):
 
@@ -43,7 +45,7 @@ class CustomerManager:
             """Creating a variable named reader which holds the csv
                DictReader object, the csv.DictReader reads the file and
                returns each row as a dictionary with the keys being the
-               column headers(usename,password)"""
+               column headers(username,password)"""
 
             # filling the customers list
             for r in reader:
@@ -57,12 +59,113 @@ class CustomerManager:
     #The below method is used to validate the customer if it's really him/her
 
     def customer_validation(self,username,password):
+
         customers = self.read_customers()
+
         for customer in customers:
             if customer["username"]==username and customer["password"]==password:
                 return True
-        
+
         return False
+
+#This class will manage the orders
+
+class OrderManager:
+
+    def __init__(self,file='orders.csv'):
+        self.file=file
+
+    #The below method will append a new customer to the csv file
+
+    def new_order(self,username,order):
+
+        with open(self.file,mode='a',newline='') as file:
+            """This will open the file: "self.file" which holds the order data
+           and mode='a' will append the data at the end of the file without
+           overwriting exisitng stuff. Newline='' will ensure that no extra lines
+           are added b/w rows. It will assign this opened file to the variable
+           file"""
+
+            writer=csv.writer(file)
+            """Creating a variable named writer which holds the csv
+               writer object, the csv.writer function creates a
+               writer object which is used to write rows into the
+               file which is passed as a parameter"""
+             
+             
+            #Below counts the number of items ordered 
+            no_of_items={}
+            for i in order:
+                item=i['Name']
+                if item in no_of_items:
+                    no_of_items[item]+=1
+                else:
+                    no_of_items[item]=1
+
+            total_cost=round(sum(float(i['Price']) for i in order),2)
+
+            summarized_order=','.join([f'{item} x{number}' for item,number in no_of_items.items()])
+            
+            writer.writerow([username,summarized_order,total_cost])
+            """This method will write a single row in the csv file
+               and the single row is the list that will be written"""
+
+    #The below method will convert the csv into a list format 
+
+    def read_orders(self):
+
+        orders=[]
+        """This will be used as a list to store order data and
+           the orders will be stored as a dictionary with username,items ordered and total cost
+           as the key"""
+        
+        with open(self.file,mode='r') as file:
+            """This will open the file: "self.file" in read mode and assigns
+           the opened file to the variable file"""
+
+            reader=csv.DictReader(file) #dictionary
+            """Creating a variable named reader which holds the csv
+               DictReader object, the csv.DictReader reads the file and
+               returns each row as a dictionary with the keys being the
+               column headers(username,items ordered,total cost)"""
+            
+            orders=list(reader) #converts the reader to a list of dictionaries
+
+        return orders
+
+    #The below method will convert only a particular customer's order into list format
+
+    def read_customer_order(self,username):
+
+        orders=[]
+
+        with open(self.file,mode='r') as file:
+            
+            reader=csv.DictReader(file)
+
+            for r in reader:
+                if r['Username']==username:
+                    orders.append(r)
+
+        return orders
+            
+    #The below method is for the display of the order
+
+    def order_display(self,string):
+        items=string.split(',')     #makes a list
+
+        display={}
+
+        for i in items:
+            name=i
+            if name in  display:
+                display[name]+=1
+            else:
+                display[name]=1
+
+        return "\n".join([f'{item} x{number}' for item,number in display.items()])
+            
+    
             
             
 
